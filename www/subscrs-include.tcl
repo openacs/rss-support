48ff852d0d22050564fafb427fbbc6227b78196f
Peter Marklund <peter_marklund@fastmail.fm>
2003-08-13 11:16:28 +0000

    adding timezone support

--- www/subscrs-include.tcl
+++ www/subscrs-include.tcl
@@ -4,7 +4,10 @@ if [info exists user_id] {
     set maybe_restrict_to_user ""
 }
 
-db_multirow subscrs get_subscrs {} {
+db_multirow -extend { last_build_pretty } subscrs get_subscrs {} {
+    set last_build_ansi [lc_time_system_to_conn $last_build_ansi]
+    set last_build_pretty [lc_time_fmt $last_build_ansi "%x %X"]
+
     if [string equal $channel_title ""] {
 	set channel_title "Subscription #$subscr_id"
     }
