05987c4d14a621aaa9c57fb1afc1360bb010e692
Jeff Davis <davis@xarg.net>
2005-02-23 12:33:22 +0000

    don't put magic text in sql, handle feeds never built without throwing an error

--- www/subscrs-include.tcl
+++ www/subscrs-include.tcl
@@ -5,8 +5,12 @@ if [info exists user_id] {
 }
 
 db_multirow -extend { lastbuild_pretty } subscrs get_subscrs {} {
-    set lastbuild_ansi [lc_time_system_to_conn $lastbuild_ansi]
-    set lastbuild_pretty [lc_time_fmt $lastbuild_ansi "%x %X"]
+    if {![empty_string_p $lastbuild_ansi]} { 
+        set lastbuild_ansi [lc_time_system_to_conn $lastbuild_ansi]
+        set lastbuild_pretty [lc_time_fmt $lastbuild_ansi "%x %X"]
+    } else { 
+        set lastbuild_pretty "never built"
+    }
 
     if [string equal $channel_title ""] {
 	set channel_title "Subscription #$subscr_id"
