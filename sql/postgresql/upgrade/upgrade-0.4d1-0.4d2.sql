d3331dc508265c4e5f9e2ffc244a8adcd457e7ab
Don Baccus <dhogaza@pacifier.com>
2009-07-30 13:23:41 +0000

    Fixed bone-headed mistake

--- sql/postgresql/upgrade/upgrade-0.4d1-0.4d2.sql
+++ sql/postgresql/upgrade/upgrade-0.4d1-0.4d2.sql
@@ -1,10 +1,11 @@
 alter table rss_gen_subscrs rename column summary_context_id to old_summary_context_id;
 
 alter table rss_gen_subscrs add summary_context_id integer
-  constraint rss_gen_subscrs_context_id_nn not null
   constraint rss_gen_subscrs_context_id_fk references acs_objects(object_id);
 
 update rss_gen_subscrs
 set summary_context_id = old_summary_context_id::integer;
 
 alter table rss_gen_subscrs drop column old_summary_context_id;
+
+alter table rss_gen_subscrs alter rss_gen_subscrs_context_id set not null;
