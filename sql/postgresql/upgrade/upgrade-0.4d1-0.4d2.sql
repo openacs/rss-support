ca2b8561499381448d2f4c4addd6d33d678c43a5
Don Baccus <dhogaza@pacifier.com>
2009-07-31 15:21:19 +0000

    Replacing this with a much simpler, smarter ALTER TABLE.

--- sql/postgresql/upgrade/upgrade-0.4d1-0.4d2.sql
+++ sql/postgresql/upgrade/upgrade-0.4d1-0.4d2.sql
@@ -1,11 +1,1 @@
-alter table rss_gen_subscrs rename column summary_context_id to old_summary_context_id;
-
-alter table rss_gen_subscrs add summary_context_id integer
-  constraint rss_gen_subscrs_context_id_fk references acs_objects(object_id);
-
-update rss_gen_subscrs
-set summary_context_id = old_summary_context_id::integer;
-
-alter table rss_gen_subscrs drop column old_summary_context_id;
-
-alter table rss_gen_subscrs alter rss_gen_subscrs_context_id set not null;
+alter table rss_gen_subscrs alter summary_context_id type integer using summary_context_id::integer;
