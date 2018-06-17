91413b0318da464bb41db8db79c5977d1146379e
Hector Romojaro <hector.romojaro@gmail.com>
2018-06-17 19:57:32 +0000

    Whitespace changes

--- tcl/rss-support-procs.tcl
+++ tcl/rss-support-procs.tcl
@@ -1,9 +1,9 @@
-# 
+#
 
 ad_library {
-    
+
     Procedure to add subscriptions
-    
+
     @author Dave Bauer (dave@thedesignexperience.org)
     @creation-date 2005-01-22
     @arch-tag: 601774f4-7b83-4eee-9b36-97c278ba1bd4
@@ -24,11 +24,11 @@ ad_proc -public ::rss_support::add_subscription {
     -context_id
     -creation_date
 } {
-     
-    
+
+
     @author Dave Bauer (dave@thedesignexperience.org)
     @creation-date 2005-01-22
-    
+
     @param summary_context_id object_id to subscribe to
     @param impl_name RssGenSubscr service contract
     implementation name
@@ -40,8 +40,8 @@ ad_proc -public ::rss_support::add_subscription {
     @param creation_date date and time subscription was created
 
     @return subscr_id
-    
-    @error 
+
+    @error
 } {
     if {![info exists context_id]} {
         set context_id $summary_context_id
@@ -55,7 +55,7 @@ ad_proc -public ::rss_support::add_subscription {
                       [list p_impl_id $impl_id] \
                       [list p_summary_context_id $summary_context_id] \
                       [list p_timeout $timeout] \
-		      [list p_lastbuild $sysdate] \
+                      [list p_lastbuild $sysdate] \
                       [list p_object_type $object_type] \
                       [list p_creation_user $creation_user ] \
                       [list p_creation_ip $creation_ip] \
@@ -66,8 +66,8 @@ ad_proc -public ::rss_support::add_subscription {
     }
     if { $lastbuild ne "" } {
         lappend var_list [list p_lastbuild $lastbuild]
-    }    
-    
+    }
+
     return [package_exec_plsql \
                 -var_list $var_list \
                 rss_gen_subscr new]
@@ -78,25 +78,23 @@ ad_proc -public rss_support::del_subscription {
     -impl_name
     -owner
 } {
-    
-    
-    
+
     @author Dave Bauer (dave@thedesignexperience.org)
     @creation-date 2005-01-23
-    
+
     @param summary_context_id summary context id to delete
- 
+
     @param impl_name implementation name to delete
 
     @param owner owner package of implementation
-    @return 
-    
-    @error 
+    @return
+
+    @error
 } {
     set subscr_id [rss_support::get_subscr_id \
                        -summary_context_id $summary_context_id \
                        -impl_name $impl_name \
-		       -owner $owner]   
+                       -owner $owner]
     set report_dir [rss_gen_report_dir -subscr_id $subscr_id]
     # remove generated RSS reports for this subscription
     file delete -force -- $report_dir
@@ -109,17 +107,17 @@ ad_proc -public rss_support::subscription_exists {
     -summary_context_id
     -impl_name
 } {
-    
+
     Check if a subscription exists
-    
+
     @author Dave Bauer (dave@thedesignexperience.org)
     @creation-date 2005-01-23
-    
+
     @param summary_context_id summary context id to check
 
-    @return 
-    
-    @error 
+    @return
+
+    @error
 } {
     return [db_string subscription_exists "" -default 0]
 }
@@ -129,20 +127,20 @@ ad_proc -public rss_support::get_subscr_id {
     -impl_name
     -owner
 } {
-    
+
     Return subscription id
-    
+
     @author Dave Bauer (dave@thedesignexperience.org)
     @creation-date 2005-02-04
-    
+
     @param summary_context_id Object_id subscribed to
 
-    @param impl_name Implementation (object_type) name 
+    @param impl_name Implementation (object_type) name
 
     @param owner Owner of implementation (package_key)
-    @return 
-    
-    @error 
+    @return
+
+    @error
 } {
     set impl_id [db_string get_impl_id ""]
     return [db_string get_subscr_id ""]
