7afddd5244609c2d90a3273b6082eb2cb070547e
Antonio Pisano <antonio@elettrotecnica.it>
2021-10-21 11:45:03 +0000

    Test rss_first_url_for_package_id
    this test will currently fail because of a bug. Fix will come in a later commit

--- tcl/test/rss-utilities-procs.tcl
+++ tcl/test/rss-utilities-procs.tcl
@@ -16,3 +16,20 @@ aa_register_case \
                 select package_id from apm_packages where package_key = 'rss-support'
             }]
     }
+
+aa_register_case \
+    -cats {api smoke} \
+    -procs {
+        rss_first_url_for_package_id
+    } \
+    rss__first_url_for_package_id {
+        Test rss_first_url_for_package_id
+    } {
+        set package_id [db_string get_mounted_package {
+            select min(object_id) from site_nodes
+        }]
+
+        aa_equals "rss_first_url_for_package_id returns the expected result" \
+            [rss_first_url_for_package_id $package_id] \
+            [lindex [site_node::get_url_from_object_id -object_id $package_id] 0]
+    }
