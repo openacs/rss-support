1103fdea556f272a5954e27bff7dffa81932d790
Gustaf Neumann <gustaf.neumann@wu-wien.ac.at>
2022-08-22 14:44:18 +0000

    preserve comments hinting to ad_apply and apply whitespace changes

--- tcl/rss-generation-service-procs.tcl
+++ tcl/rss-generation-service-procs.tcl
@@ -70,6 +70,7 @@ ad_proc -public rss_gen_report {
             set $name $val
         }
     }
+    #ns_log notice "FORMER ad_apply [list rss_gen {*}$args]"
     set xml [rss_gen {*}$args]
 
     # Write report.
